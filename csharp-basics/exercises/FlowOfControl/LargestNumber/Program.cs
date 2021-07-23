485e05b20ed3dac504faeb9e11d04c1f96b4f537
JL <JL@MSI>
2021-07-23 15:36:35 +0300

    Please ignore exercise 4 and 5 folders, I wasn't able to delete them from this branch without invoking a change (spent a few hours doing that without any luck). The 4th and 5th exercises are located in the folders prefaced with "Real". Also, for exercise 5 I was not able to find a way to include a nested "if" statement (in addition to using the switch statement) which was not ambigious in nature, i.e. if (input != int) or something of the nature.

---
+++ csharp-basics/exercises/FlowOfControl/LargestNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        //TODO: Write a C# program to to find the largest of three numbers.
+        static void Main(string[] args)
+        {
+            int input1, input2, input3;
+
+            Console.WriteLine("Input the 1st number: ");
+            int.TryParse(Console.ReadLine(), out input1);      
+
+            Console.WriteLine("Input the 2nd number: ");
+            int.TryParse(Console.ReadLine(), out input2);
+
+            Console.WriteLine("Input the 3rd number: ");
+            int.TryParse(Console.ReadLine(), out input3);
+
+            if (input1 > input2 && input1 > input3) Console.WriteLine($"{input1} is the largest number");
+            if (input2 > input1 && input2 > input3) Console.WriteLine($"{input2} is the largest number");
+            if (input3 > input2 && input3 > input1) Console.WriteLine($"{input3} is the largest number");
+
+            Console.ReadKey();
+        }
+    }
+}
